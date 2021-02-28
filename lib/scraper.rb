15b2ec528f3c9f857c46533e291b26daa6b2324a
Lena Hou <lenahou2013@gmail.com>
2021-02-28 18:17:29 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,8 +8,8 @@ class Scraper
     doc = Nokogiri::HTML(open(index_url))
     doc.css("div.student-card").each do |card|
       student_hash = {
-        name: student.css("h4.student-name").text,
-        location: student.css("p.student_location").text,
+        name: card.css("h4.student-name").text,
+        location: card.css("p.student_location").text,
         profile_url: "http://students.learn.co/#{student.css("a").attribute("href").value}"
       }
       student_info << student_hash
