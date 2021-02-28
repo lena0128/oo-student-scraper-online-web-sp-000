7692e52b5b2ba1c0a3addac493d93ff8fa1a63bb
Lena Hou <lenahou2013@gmail.com>
2021-02-28 18:51:55 +0000

    Automatically backed up by Learn

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -26,7 +26,7 @@ class CommandLineInterface
 
   def display_students
     Student.all.each do |student|
-      puts "#{student.name.upcase}".colorize(:orange)
+      puts "#{student.name.upcase}".colorize(:light_magenta)
       puts "  location:".colorize(:light_blue) + " #{student.location}"
       puts "  profile quote:".colorize(:light_blue) + " #{student.profile_quote}"
       puts "  bio:".colorize(:light_blue) + " #{student.bio}"
