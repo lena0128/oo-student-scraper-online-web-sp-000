3df8dd6e9d0398d5c54d62d1ed2cf8b4cfd841d1
Lena Hou <lenahou2013@gmail.com>
2021-02-28 17:58:26 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,21 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    students_info = []
+    html = Notogiri::HTML(open(index_url))
+    html.css(".student-card").map do |student|
+      student_hash = {
+        name: student.css("h4.student-name").text,
+        location: student.css("p.student_location").text
+        profile_url: "https://learn-co-curriculum.github.io/student-scraper-test-page/students/" + student.css("a").attribute("href")
+      }
+      student_info << student_hash
   end
+  student_hash
+end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
